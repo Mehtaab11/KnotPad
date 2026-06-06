24843f13bec6b3b7ce892bc2361b62c97610440e
Mehtaab <mehtaabaalam117@gmail.com>
2026-06-06 12:51:57 +0530

    Added Register and login routes with middleware authentication

---
+++ server/models/user.js
@@ -0,0 +1,23 @@
+import { model, Schema } from "mongoose"
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: String,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    }
+}, { timestamps: true })
+
+const User = model("User", UserSchema)
+export default User
